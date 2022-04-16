c9802dadccc6130db219599632176d6439af45d6
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-04-16 23:26:29 +0300

    add new page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <p>
+        Hello, I am Ali Al-Shaikh from Qatif, Saudi Arabia. I have my own blog
+        at <a href="https://t.me/mi3li_blog">@mi3li_blog</a>. I am a web
+        developer and I became Telegram Web Bot developer now, thanks for
+        telegram team!
+      </p>
+    </div>
+  );
+}
