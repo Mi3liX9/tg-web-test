b9fbe4bcc3bf90210311bf4c41c2dc283a1054b6
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-04-16 23:29:08 +0300

    add about page link

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
@@ -5,6 +7,9 @@ export default function Home() {
         Hello There, This is <a href="https://t.me/mi3lix9">@mi3lix9</a> first
         Next.js Telegram web bot!
       </p>
+      <Link href="/about" passHref>
+        <button>About page</button>
+      </Link>
     </div>
   );
 }
