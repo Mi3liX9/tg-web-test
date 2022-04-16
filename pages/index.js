2e2b3f30adcc5245636f839b036517973a508bbb
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-04-17 01:30:27 +0300

    predict name

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -26,15 +27,32 @@ export default function Home() {
 
   return (
     <div dir="rtl">
+      <h2>السلام عليكم</h2>
       <p>
         السلام عليكم، معكم علي الشيخ <a href="https://t.me/mi3lix9">@mi3lix9</a>{" "}
         وهذا اول تطبيق ويب بوت اقوم بانشاءه!
       </p>
       <Link href="/about" passHref>
-        <button>حول</button>
+        <button>حول علي الشيخ</button>
       </Link>
 
+      <h2>سأخمن من أنت</h2>
+      <p>
+        حياك الله استاذ {window?.Telegram?.WebApp?.initData?.user?.first_name}
+      </p>
+      <img
+        src={window?.Telegram?.WebApp?.initData?.user?.photo_url}
+        alt={window?.Telegram?.WebApp?.initData?.user?.first_name}
+      />
+      <p>
+        باستخدام الواجهة البرمجية للويب بوت استطيع معرفى بعض معلومات المستخدم،
+        بعضها يكون بطلب صلاحيات مثل الموقع والكاميرا وبعضها لا، مثل بروفايل
+        المستخدم والثيم
+      </p>
+
+      <h2>نظرة على الويب بوت</h2>
       <p>دعنا نلقي نظرة عمّا نستطيع عمله مع الويب بوت:</p>
+
       <button onClick={toggleMainButton}>عرض أو اخفاء الزر السفلي</button>
     </div>
   );
