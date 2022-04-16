7745ee03e3f96904f6c458f0f06b87532427e34b
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-04-17 02:22:30 +0300

    fix: tgApp page

--- pages/pre.js
+++ pages/pre.js
@@ -1,3 +1,9 @@
 export default function Pre() {
+  const [tgApp, setTgApp] = useState(undefined);
+
+  useEffect(() => {
+    setTgApp(window.Telegram.WebApp);
+  }, []);
+
   return <pre dir="ltr">{JSON.stringify(tgApp, null, 2)}</pre>;
 }
