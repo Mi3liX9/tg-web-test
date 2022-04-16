8a7a9bd4dc3f5aa87e6b5ee6a2db962826764fe8
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-04-16 19:24:00 +0300

    initialize

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="beforeInteractive"
+        src="https://telegram.org/js/telegram-web-app.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
