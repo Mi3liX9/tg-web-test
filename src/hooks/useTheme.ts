d5c0de45edd37ebf20ee3c77903a181099cc7367
Ali Al-Shaikh <mi3lix9@outlook.sa>
2022-04-17 23:31:44 +0300

    try fix telegram theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -3,11 +3,7 @@ import { useEffect } from "react";
 export function useTheme() {
   useEffect(() => {
     if (typeof window === "undefined") {
-    } else if (
-      typeof (window as any).Telegram.WebApp.initDataUnsafe.user === undefined
-    ) {
-      console.log((window as any).Telegram.WebApp.initDataUnsafe.user);
-
+    } else if ((window as any).Telegram.WebApp.initData !== "") {
       document.body.className = "telegram";
     } else if (
       window.matchMedia &&
